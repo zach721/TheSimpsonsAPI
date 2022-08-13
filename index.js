c2f1fb70854d491c07df1b7cf190fc3686675195
Zakarias <46316857+zach721@users.noreply.github.com>
2022-08-13 10:17:21 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
+const api = require("./api/api")
+
+app.use(express.json({extended:false}))
+app.use("/api/api",api)
 
 app.get('/', function (req, res) {
   res.send(api)
 })
 
 app.listen(3000)
-
-module.exports = express
